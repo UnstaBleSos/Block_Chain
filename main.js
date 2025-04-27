fa37988fcb86b2fce643a8d3ab61400e7766fd62
UnstaBleSos <Rambahadru@gmail.com>
2025-04-27 17:43:52 +0545

    Proof of work

--- main.js
+++ main.js
@@ -7,15 +7,24 @@ class Block{
         this.data=data
         this.previousHash=previousHash
         this.hash = this.calculateHash()
+        this.nonce = 0
     }
     calculateHash(){
-        return SHA256(this.index+this.previousHash+this.timestamp+JSON.stringify(this.data)).toString()
+        return SHA256(this.index+this.previousHash+this.timestamp+JSON.stringify(this.data)+this.nonce).toString()
+    }
+    mineBlock(difficulty){
+        while(this.hash.substring(0,difficulty) !== Array(difficulty+1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block mined" + this.hash)
     }
 }
 
 class BlockChain{
     constructor(){
         this.chain = [this.createGenesisBlock()]
+        this.difficulty=4
     }
 
     createGenesisBlock(){
@@ -27,7 +36,7 @@ class BlockChain{
     }
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash
-        newBlock.hash = newBlock.calculateHash()
+        newBlock.mineBlock(this.difficulty)
         this.chain.push(newBlock)
     }
 
@@ -52,10 +61,9 @@ class BlockChain{
 }
 
 const coin = new BlockChain
+console.log("Mining Block 1....")
 coin.addBlock(new Block(1,"11/11/1111",{amount:4}))
+
+console.log("Mining Block 2....")
 coin.addBlock(new Block(2,"12/12/1212",{amount:10}))
 
-console.log("BlockChain Valid? "+ coin.isChainValid())
-
-
-// console.log(JSON.stringify(coin,null,4))
